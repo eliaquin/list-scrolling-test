b393e3ba8532b61b7be2dc02b932ec6220882524
Eliaquín Encarnación <eliaquin@gmail.com>
2020-04-10 22:02:40 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './data.json';
 
-function App() {
+const App = () => {
+  const refs = data.reduce((acc, value) => {
+    acc[value.id] = React.createRef();
+    return acc;
+  }, {});
+
+  const renderData = () => {
+    return data.map(el => <tr key={el.id} ref={refs[el.id]}>
+      <td>{el.id}</td>
+      <td>{el.first_name}</td>
+      <td>{el.last_name}</td>
+      <td>{el.email}</td>
+      <td>{el.gender}</td>
+    </tr>)
+  }
+
+  const handleClick = id =>
+    refs[id].current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul style={styles.buttonContainer}>
+        {data.map(item => (
+          <span key={item.id} style={styles.idSpan}>
+            <span
+              type="button"
+              onClick={() => handleClick(item.id)}
+            >
+              {item.id}
+            </span>
+          </span>
+        ))}
+      </ul>
+      <div style={styles.scrollableContainer}>
+        <table>
+          <tbody>
+            {renderData()}
+          </tbody>
+        </table>
+      </div>
     </div>
+
   );
+}
+
+const styles = {
+  scrollableContainer: {
+    height: 500,
+    width: 600,
+    border: '1px solid gray',
+    marginTop: 20,
+    overflowX: 'hidden',
+    overflowY: 'auto',
+  },
+  idSpan: {
+    marginLeft: 6,
+  },
+  buttonContainer: {
+    width: 500,
+    display: 'flex',
+    flexWrap: 'wrap',
+    cursor: 'pointer',
+  }
 }
 
 export default App;
